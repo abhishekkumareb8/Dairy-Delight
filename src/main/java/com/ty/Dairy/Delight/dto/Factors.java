8c1ac7ca3b104a067099de2d56b84e2b3154f1eb
Rukmesh Srinivas <rukmesh.9@gmail.com>
2023-02-16 17:11:23 +0530

    Added Factors DTO-Service

---
+++ src/main/java/com/ty/Dairy/Delight/dto/Factors.java
@@ -0,0 +1,16 @@
+package com.ty.Dairy.Delight.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Factors {
+
+	@Id
+	private int id;
+	private float fat;
+	private float SNF;
+
+}
---
+++ src/main/java/com/ty/Dairy/Delight/repository/FactorsRepository.java
@@ -0,0 +1,9 @@
+package com.ty.Dairy.Delight.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.Dairy.Delight.dto.Factors;
+
+public interface FactorsRepository extends JpaRepository<Factors, Integer> {
+
+}
